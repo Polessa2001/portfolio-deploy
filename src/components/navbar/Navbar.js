e87c4288b9079f27d09bcda3dbe660bc8ebfabf9
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-20 10:11:54 +0200

    work with responsivness 1 and 2 page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,28 +2,38 @@ import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './Navbar.scss';
 import menu from '../../assets/menu.png';
+import { useSpring, animated } from '@react-spring/web';
+
 const Navbar = () => {
-  const [isMenuVisible, setIsMenuVisible] = useState(true);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+
   const showMenu = () => {
-    setTimeout(() => {
-      setIsMenuVisible(!isMenuVisible);
-    }, 1000);
-  }
+    setIsMenuVisible(!isMenuVisible);
+  };
+  const styles = useSpring({
+    reset: true,
+    from: { opacity: '0' },
+    to: { opacity: '1' },
+    config: { duration: '600' },
+  });
   return (
     <div>
-      <img src = {menu} className="menu" onClick={showMenu} alt=""/>
-      <div className={`${isMenuVisible ? 'navbar' : 'hidden'}`}>
-        <div className="links">
-          <a href="#section-2">About</a>
-          <a href="#section-3">Skills</a>
-          <a href="#section-4">Projects</a>
-          <a href="#section-5">Contact</a>
+      <img src={menu} className="menu" onClick={showMenu} alt="" />
+      <animated.div style={styles}>
+        <div className={`navbar ${isMenuVisible ? 'visible' : 'hidden'}`}>
+          <div className="links">
+            <a href="#section-2">About</a>
+            <a href="#section-3">Skills</a>
+            <a href="#section-4">Projects</a>
+            <a href="#section-5">Contact</a>
+          </div>
+          <a href="section">
+            <img src={logo} alt="logo" />
+          </a>
         </div>
-        <a href="section">
-          <img src={logo} alt="logo"/>
-        </a>
-      </div>
-      <img src={logo} alt="logo" className='logoMobile' />
+        </animated.div>
+      <img src={logo} alt="logo" className={`logoMobile ${isMenuVisible ? 'visible' : ''}`} />
     </div>
   );
 };
