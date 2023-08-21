fdff56ccfcdf4b8d0ead0466965fe37b27b56512
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-21 12:40:02 +0200

    section 2 little fix

--- src/data/AboutMeCardsData.js
+++ src/data/AboutMeCardsData.js
@@ -8,13 +8,13 @@ const AboutMeCardsData = [
     content: "My story as a developer began in 2018 when I decided to pursue a bachelor's degree in Engineering. " +
       "Ever since I have been interested in technologies and mathematics. It helped me formulate logical thinking" +
       " and analysis. After completing university, I started learning coding " +
-      "on my own. Later, my desire to enhance my skills motivated me to " +
-      "enroll in Le Wagon bootcamp",
+      "on my own.",
     img: aboutone
   },
   {
     id: 2,
-    content: "Studying there further developed me as a professional. I felt in my element, and I was 100% sure that I am " +
+    content: "Later, my desire to enhance my skills motivated me to " +
+      "enroll in Le Wagon bootcamp Studying there further developed me as a professional. I felt in my element, and I was 100% sure that I am " +
       "following the right direction. The creative aspect of me, such as understanding design basics, color matching, " +
       "and technical background, helped me immensely on my journey.",
     img: abouttwo
