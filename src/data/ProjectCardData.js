78075e2933cd252b9519f333d01652f3544f6414
Polina Sokolova <polina2001sokolovas@gmail.com>
2024-02-01 20:42:07 +0100

    changed CV

--- src/data/ProjectCardData.js
+++ src/data/ProjectCardData.js
@@ -5,7 +5,7 @@ const ProjectCardData = [
   { name: "WTT", backgroundColor: green, gcs: '3', gce: '4',grs:'1', gre: '2',link:"https://troopl.com/polinasokolovadev/wtt-games" },
   { name: "WatchList", backgroundColor: yellow, gcs: '4', gce: 'end',grs:'1', gre: '2',link:"https://troopl.com/polinasokolovadev/watch-list" },
   { name: "SoundApp", backgroundColor: lightGreen, gcs: '3', gce: 'end',grs:'2', gre: '3',link:"https://troopl.com/polinasokolovadev/soundapp"},
-  { name: "Fintess", backgroundColor: green, gcs: '1', gce: '2',grs:'3', gre: '4', link:"https://fitness-center-ms.tilda.ws/"},
+  { name: "Fintess", backgroundColor: green, gcs: '1', gce: '2',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/fintess-center"},
   { name: "MouseTrap Game", backgroundColor: yellow,gcs: '2', gce: '4',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/mouse-trapper"},
   { name: "Casino", backgroundColor: lightGreen, gcs: '4', gce: 'end',grs:'3', gre: '4', link:"https://test-casino-b50e6c28aad1.herokuapp.com/"},
 ];
