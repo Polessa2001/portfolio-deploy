ee586c2ddd31859f70b92280f88659fb0132401e
Polina Sokolova <polina2001sokolovas@gmail.com>
2024-02-08 16:31:06 +0100

    cv new and promptcraft

--- src/data/ProjectCardData.js
+++ src/data/ProjectCardData.js
@@ -3,7 +3,7 @@ import { green, lightGreen, yellow, blue } from '../styles/colors';
 const ProjectCardData = [
   { name: "PlantAqua", backgroundColor: blue ,gcs: '1', gce: '3',grs:'1', gre: '3',link:"https://troopl.com/polinasokolovadev/plantaqua"},
   { name: "WTT", backgroundColor: green, gcs: '3', gce: '4',grs:'1', gre: '2',link:"https://troopl.com/polinasokolovadev/wtt-games" },
-  { name: "WatchList", backgroundColor: yellow, gcs: '4', gce: 'end',grs:'1', gre: '2',link:"https://troopl.com/polinasokolovadev/watch-list" },
+  { name: "PromptCraft", backgroundColor: yellow, gcs: '4', gce: 'end',grs:'1', gre: '2',link:"https://promptcraft-5jae.vercel.app/" },
   { name: "SoundApp", backgroundColor: lightGreen, gcs: '3', gce: 'end',grs:'2', gre: '3',link:"https://troopl.com/polinasokolovadev/soundapp"},
   { name: "Fintess", backgroundColor: green, gcs: '1', gce: '2',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/fintess-center"},
   { name: "MouseTrap Game", backgroundColor: yellow,gcs: '2', gce: '4',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/mouse-trapper"},
