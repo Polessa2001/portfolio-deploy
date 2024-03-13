07669f7e5c5c094e0847cfe48034fb416f706222
Polina Sokolova <polina2001sokolovas@gmail.com>
2024-03-13 12:06:15 +0100

    d

--- src/data/ProjectCardData.js
+++ src/data/ProjectCardData.js
@@ -6,7 +6,7 @@ const ProjectCardData = [
   { name: "PromptCraft", backgroundColor: yellow, gcs: '4', gce: 'end',grs:'1', gre: '2',link:"https://promptcraft-5jae.vercel.app/" },
   { name: "SoundApp", backgroundColor: lightGreen, gcs: '3', gce: 'end',grs:'2', gre: '3',link:"https://troopl.com/polinasokolovadev/soundapp"},
   { name: "Fintess", backgroundColor: green, gcs: '1', gce: '2',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/fintess-center"},
-  { name: "MouseTrap Game", backgroundColor: yellow,gcs: '2', gce: '4',grs:'3', gre: '4', link:"https://troopl.com/polinasokolovadev/mouse-trapper"},
+  { name: "Ecommerce store", backgroundColor: yellow,gcs: '2', gce: '4',grs:'3', gre: '4', link:"https://ecommerce2-omega.vercel.app/"},
   { name: "Casino", backgroundColor: lightGreen, gcs: '4', gce: 'end',grs:'3', gre: '4', link:"https://test-casino-b50e6c28aad1.herokuapp.com/"},
 ];
 
